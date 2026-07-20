ca29070c2ce6fd5b43002f0704115450e30ef768
Akash Torath <takasht20@gmail.com>
2026-07-20 12:07:30 +0530

    added redis count instializer in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,14 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { connectRedis } from "./config/redis.js";
-
+import redisCounterService from "./services/redis-counter.service.js";
 const startServer = async () => {
     try {
         await connectDB();
 
         await connectRedis();
+
+        await redisCounterService.initializeCounter();
 
         app.listen(env.PORT, () => {
             console.log(`🚀 Server running on ${env.PORT}`);

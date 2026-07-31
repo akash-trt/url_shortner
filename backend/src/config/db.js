655d81743f10b3cf9e9f17c4ac6154801aa53d64
Akash Torath <takasht20@gmail.com>
2026-07-31 15:07:42 +0530

    Ready for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,40 @@
+// import mongoose from "mongoose";
+// import { env } from "./env.js";
+
+// export const connectDB = async () => {
+//     try {
+//         console.log("Using URI:", process.env.MONGO_URI?.replace(/\/\/.*:.*@/, "//<user>:<password>@"));
+//         await mongoose.connect(env.MONGO_URI);
+
+//         console.log("✅ MongoDB Connected");
+//     } catch (error) {
+//         console.error("❌ MongoDB Connection Failed");
+//         console.error(error);
+
+//         process.exit(1);
+//     }
+// };
+
+import dns from "node:dns";
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
+// Force Node to use Google's DNS servers
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4",
+]);
+
 export const connectDB = async () => {
     try {
+        console.log("DNS Servers:", dns.getServers());
+
         await mongoose.connect(env.MONGO_URI);
 
         console.log("✅ MongoDB Connected");
     } catch (error) {
         console.error("❌ MongoDB Connection Failed");
-        console.error(error.message);
+        console.error(error);
 
         process.exit(1);
     }

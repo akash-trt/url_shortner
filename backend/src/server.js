adcfa0aca6265403d1fc234647df3e541681f3ae
Akash Torath <takasht20@gmail.com>
2026-07-31 17:32:34 +0530

    merged worker and server start

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { env } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { connectRedis } from "./config/redis.js";
 import redisCounterService from "./services/redis-counter.service.js";
+
 const startServer = async () => {
     try {
         await connectDB();
@@ -12,12 +13,17 @@ const startServer = async () => {
 
         await redisCounterService.initializeCounter();
 
+        // Start BullMQ worker
+        await import("./workers/analytics.worker.js");
+
+        console.log("🚀 Analytics Worker Started");
+
         app.listen(env.PORT, () => {
             console.log(`🚀 Server running on ${env.PORT}`);
         });
     } catch (err) {
         console.error(err);
-
+        
         process.exit(1);
     }
 };
